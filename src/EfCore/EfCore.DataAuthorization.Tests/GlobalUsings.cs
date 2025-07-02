697ae9927579a58cce0fd695ebcbfa7ab52dd315
Steven Hoang <hoangbaoduy@gmail.com>
2025-07-02 09:27:46 +0800

    move project to src folder

--- src/EfCore/EfCore.DataAuthorization.Tests/GlobalUsings.cs
+++ src/EfCore/EfCore.DataAuthorization.Tests/GlobalUsings.cs
@@ -1,7 +1,4 @@
 // Global using directives
-
-global using System.Linq;
-global using System.Threading.Tasks;
 global using AutoBogus;
 global using EfCore.DataAuthorization.Tests.TestEntities;
 global using Microsoft.Extensions.DependencyInjection;
