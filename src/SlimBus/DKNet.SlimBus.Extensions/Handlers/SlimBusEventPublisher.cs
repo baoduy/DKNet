b10de94f411e3a4426adb0ffc8f9febdf222756a
Steven Hoang <drunkcoding@outlook.com>
2025-09-21 13:20:56 +0800

    update unit tests

--- src/SlimBus/DKNet.SlimBus.Extensions/Handlers/SlimBusEventPublisher.cs
+++ src/SlimBus/DKNet.SlimBus.Extensions/Handlers/SlimBusEventPublisher.cs
@@ -7,13 +7,11 @@ public class SlimBusEventPublisher(IMessageBus bus) : IEventPublisher
 {
     public virtual Task PublishAsync(object eventObj, CancellationToken cancellationToken = default)
     {
-        if (eventObj is IEventItem item)
-        {
-            var headers =
-                item.AdditionalData.ToDictionary(i => i.Key, object (v) => v.Value, StringComparer.OrdinalIgnoreCase);
-            return bus.Publish(item, headers: headers, cancellationToken: cancellationToken);
-        }
+        if (eventObj is not IEventItem item) return bus.Publish(eventObj, cancellationToken: cancellationToken);
 
-        return bus.Publish(eventObj, cancellationToken: cancellationToken);
+        var headers =
+            item.AdditionalData.ToDictionary(i => i.Key, object (v) => v.Value, StringComparer.OrdinalIgnoreCase);
+        return bus.Publish(item, headers: headers, cancellationToken: cancellationToken);
+
     }
 }
