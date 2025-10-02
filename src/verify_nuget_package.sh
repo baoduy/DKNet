e58cf76b9d477588f662598dbf6387619b6d9bb6
Steven Hoang <drunkcoding@outlook.com>
2025-10-02 19:42:30 +0800

    include source into nuget packages

--- src/verify_nuget_package.sh
+++ src/verify_nuget_package.sh
@@ -1,3 +1,4 @@
 # Build and pack the DKNet.FW solution into a NuGet package
 # Verify the package using this website https://nuget.info/packages
+rm -f ./nupkgs/*
 dotnet pack DKNet.FW.sln --configuration Release --output ./nupkgs -p:PackageVersion=9.0.0
