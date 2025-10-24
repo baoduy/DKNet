e6d231cac92ea3b38951066fe0668dcf9408a166
Steven Hoang <drunkcoding@outlook.com>
2025-10-24 13:43:44 +0800

    improve global filter

--- src/EfCore/DKNet.EfCore.DataAuthorization/Internals/DataOwnerAuthQueryRegister.cs
+++ src/EfCore/DKNet.EfCore.DataAuthorization/Internals/DataOwnerAuthQueryRegister.cs
@@ -1,8 +1,9 @@
 ﻿using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 using DKNet.EfCore.Extensions.Configurations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DKNet.EfCore.DataAuthorization.Internals;
 
@@ -18,56 +19,23 @@ namespace DKNet.EfCore.DataAuthorization.Internals;
 /// </remarks>
 [SuppressMessage("Major Code Smell",
     "S3011:Reflection should not be used to increase accessibility of classes, methods, or fields")]
-internal sealed class DataOwnerAuthQueryRegister : IGlobalQueryFilterRegister
+internal sealed class DataOwnerAuthQueryRegister : GlobalQueryFilterRegister
 {
-    private static readonly MethodInfo Method = typeof(DataOwnerAuthQueryRegister)
-        .GetMethod(nameof(ApplyQueryFilter), BindingFlags.Static | BindingFlags.NonPublic)!;
-
-
-    /// <summary>
-    ///     Applies the global query filters to the model builder.
-    /// </summary>
-    /// <param name="modelBuilder">The model builder to configure.</param>
-    /// <param name="context">The database context.</param>
-    public void Apply(ModelBuilder? modelBuilder, DbContext context)
+    protected override void HasQueryFilter<TEntity>(EntityTypeBuilder<TEntity> builder, DbContext context)
     {
-        if (modelBuilder == null)
+        if (context is not IDataOwnerDbContext dataOwnerContext)
         {
-            Debug.WriteLine($"{nameof(DataOwnerAuthQueryRegister)}-Ignored: ModelBuilder is null.");
+            Debug.Fail("The DbContext must implement IDataOwnerDbContext to use DataOwnerAuthQueryRegister.");
             return;
         }
 
-        if (context is not IDataOwnerDbContext keyContext)
-        {
-            Debug.WriteLine(
-                $"{nameof(DataOwnerAuthQueryRegister)}-Ignored: DbContext is not an implementation of IDataKeyContext.");
-            return;
-        }
+        builder.HasQueryFilter(x =>
+            dataOwnerContext.AccessibleKeys.Count == 0 ||
+            dataOwnerContext.AccessibleKeys.Contains(((IOwnedBy)x).OwnedBy));
+    }
 
-        var entityTypes = modelBuilder.Model.GetEntityTypes()
+    protected override IEnumerable<IMutableEntityType> GetEntityTypes(ModelBuilder modelBuilder) =>
+        modelBuilder.Model.GetEntityTypes()
             .Where(t => typeof(IOwnedBy).IsAssignableFrom(t.ClrType))
             .Where(t => t.GetDiscriminatorValue() == null);
-
-        foreach (var entityType in entityTypes)
-        {
-            var genericMethod = Method.MakeGenericMethod(entityType.ClrType);
-            genericMethod.Invoke(null, [modelBuilder, keyContext]);
-        }
-    }
-
-
-    /// <summary>
-    ///     Applies the ownership query filter to a specific entity type.
-    /// </summary>
-    /// <typeparam name="TEntity">The type of entity to filter.</typeparam>
-    /// <param name="modelBuilder">The model builder instance.</param>
-    /// <param name="context">The data owner context.</param>
-    [SuppressMessage("Usage", "MA0002:The IEqualityComparer<string> is not compatible with EfCore.")]
-    private static void ApplyQueryFilter<TEntity>(ModelBuilder modelBuilder, IDataOwnerDbContext context)
-        where TEntity : class, IOwnedBy
-    {
-        //TODO: convert to named query filter when migrate to EFCore 10
-        modelBuilder.Entity<TEntity>()
-            .HasQueryFilter(x => !context.AccessibleKeys.Any() || context.AccessibleKeys.Contains(x.OwnedBy));
-    }
 }
