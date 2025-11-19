1922929f8023b507cc7307eb828f6a1c30373d9a
Steven Hoang <drunkcoding@outlook.com>
2025-11-19 12:10:42 +0800

    up

--- src/verify_nuget_package.sh
+++ src/verify_nuget_package.sh
@@ -2,8 +2,7 @@
 # Build and pack the DKNet.FW solution into a NuGet package
 # Verify the package using this website https://nuget.info/packages
 
-# Generate version based on current date: 9.0.YYMMDD
-VERSION="9.9.$(date +%y%m%d)"
+VERSION="10.0.$(date +%y%m%d)"
 
 echo "Generating NuGet packages with version: $VERSION"
 
